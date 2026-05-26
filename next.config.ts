f8c0cedcec5c3ec4891f01ff1e7a4ff7d1591046
Krishna Malak <99009294+krishnavmalak@users.noreply.github.com>
2026-05-26 20:21:11 +0530

    Fix next config turbopack types

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
-  experimental: {
-    turbopack: {
-      root: "./",
-    },
-  },
 };
 
 export default nextConfig;
